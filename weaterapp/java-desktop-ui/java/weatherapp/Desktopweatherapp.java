f89340deb9c7a400f9c47373241bd5476896864f
minahil77 <162546320+minahil77@users.noreply.github.com>
2024-03-22 13:23:01 +0500

    Update Desktopweatherapp.java

--- weaterapp/java-desktop-ui/java/weatherapp/Desktopweatherapp.java
+++ weaterapp/java-desktop-ui/java/weatherapp/Desktopweatherapp.java
@@ -2,11 +2,11 @@ package sda.sda_project;
 
 import javax.swing.*;
 
-public class weatherAppGui extends JFrame{
+public class Desktopweatherapp extends JFrame{
     
     public weatherAppGui(){
        setDefaultCloseOperation(EXIT_ON_CLOSE);
-       setSize (600,700);
+       setSize (1200,800);
        setLocationRelativeTo(null);
        setLayout(null);
        
