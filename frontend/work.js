948149a008daab824cc6d47c7c67e10b143bd1a0
minahil77 <162546320+minahil77@users.noreply.github.com>
2024-03-24 16:59:24 +0500

    Update work.js

--- frontend/work.js
+++ frontend/work.js
@@ -1,3 +1,4 @@
+   
 document.getElementById('city').addEventListener('input', function () {
 var city = this.value;
 getWeather(city);
@@ -55,3 +56,48 @@ try {
           dailyForecast[day].maxTemp = Math.max(dailyForecast[day].maxTemp, data.main.temp_max);
       }
   });
+
+  document.querySelector('.date-dayname').textContent = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+
+  const date = new Date().toUTCString();
+  const extractedDateTime = date.slice(5, 16);
+  document.querySelector('.date-day').textContent = extractedDateTime.toLocaleString('en-US');
+
+  const currentWeatherIconCode = dailyForecast[new Date().toLocaleDateString('en-US', { weekday: 'long' })].icon;
+  const weatherIconElement = document.querySelector('.weather-icon');
+  weatherIconElement.innerHTML = getWeatherIcon(currentWeatherIconCode);
+
+  document.querySelector('.location').textContent = response.data.city.name;
+  document.querySelector('.weather-desc').textContent = dailyForecast[new Date().toLocaleDateString('en-US', { weekday: 'long' })].description.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+
+  document.querySelector('.humidity .value').textContent = dailyForecast[new Date().toLocaleDateString('en-US', { weekday: 'long' })].humidity + ' %';
+  document.querySelector('.wind .value').textContent = dailyForecast[new Date().toLocaleDateString('en-US', { weekday: 'long' })].windSpeed + ' m/s';
+
+
+  const dayElements = document.querySelectorAll('.day-name');
+  const tempElements = document.querySelectorAll('.day-temp');
+  const iconElements = document.querySelectorAll('.day-icon');
+
+  dayElements.forEach((dayElement, index) => {
+      const day = Object.keys(dailyForecast)[index];
+      const data = dailyForecast[day];
+      dayElement.textContent = day;
+      tempElements[index].textContent = `${Math.round(data.minTemp)}º / ${Math.round(data.maxTemp)}º`;
+      iconElements[index].innerHTML = getWeatherIcon(data.icon);
+  });
+
+} catch (error) {
+  console.error('Məlumat alınarkən səhv baş verdi:', error.message);
+}
+}
+
+function getWeatherIcon(iconCode) {
+const iconBaseUrl = 'https://openweathermap.org/img/wn/';
+const iconSize = '@2x.png';
+return `<img src="${iconBaseUrl}${iconCode}${iconSize}" alt="Weather Icon">`;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+getWeather();
+setInterval(getWeather, 900000);
+});
