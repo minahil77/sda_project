15329067e8aebb9b15a153d808efcbbd70ff54a7
M Awais Ali <102896863+awaisali7@users.noreply.github.com>
2024-03-27 11:21:14 +0500

    Update serverSide.java

--- Java Resources/src/main/java/serverSide.java
+++ Java Resources/src/main/java/serverSide.java
@@ -11,11 +11,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.TimeZone;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.JsonElement;
 
 import com.google.gson.Gson;
@@ -116,10 +118,102 @@ public class serverSide extends HttpServlet {
         request.setAttribute("weatherData", saveResponse.toString());
         request.getRequestDispatcher("internal.jsp").forward(request, response);
         
+ //air pollution
+        String air_url="http://api.openweathermap.org/data/2.5/air_pollution?lat="+latitude+"&lon="+longt+"&appid="+key;
+        URL airApi=new URL (air_url);
+		HttpURLConnection connection2 = (HttpURLConnection) airApi.openConnection();
+           connection2.setRequestMethod("GET");
+        InputStream inputConnection2 = connection2.getInputStream();
+        InputStreamReader read_information2 = new InputStreamReader(inputConnection2);
         
+        
+    
+        StringBuilder saveResponse2 = new StringBuilder();
+        Scanner scanner2 = new Scanner(read_information2);
+        
+        while (scanner2.hasNext()) {
+        	 saveResponse2.append(scanner2.nextLine());
+        }
+        System.out.println(saveResponse2);
+        Gson gson2 = new Gson();
+        JsonObject jsonObject2 = gson.fromJson(saveResponse2.toString(), JsonObject.class);
+        scanner2.close();
+        
+           
+          
+        double co = jsonObject2.getAsJsonArray("list").get(0).getAsJsonObject().getAsJsonObject("components").getAsJsonObject().get("co").getAsDouble();
+        double no = jsonObject2.getAsJsonArray("list").get(0).getAsJsonObject().getAsJsonObject("components").getAsJsonObject().get("no").getAsDouble();
+//        double no2 = jsonObject2.getAsJsonArray("list").get(0).getAsJsonObject().get("components").getAsJsonObject().get("no2").getAsDouble();
+//        double o3 = jsonObject2.getAsJsonArray("list").get(0).getAsJsonObject().get("components").getAsJsonObject().get("o3").getAsDouble();
+//        double so2 = jsonObject2.getAsJsonArray("list").get(0).getAsJsonObject().get("components").getAsJsonObject().get("so2").getAsDouble();
+
+          
+//          long co=jsonObject2.getAsJsonObject("components").get("co").getAsLong();
+//          long no=jsonObject2.getAsJsonObject("components").get("no").getAsLong();
+//          long no2=jsonObject2.getAsJsonObject("components").get("no2").getAsLong();
+//          long o3=jsonObject2.getAsJsonObject("components").get("o3").getAsLong();
+//          long so2=jsonObject2.getAsJsonObject("components").get("so2").getAsLong();
+          
+          request.setAttribute("co", co);
+          request.setAttribute("no",no);
+          request.setAttribute("city", userInput);
+          request.setAttribute("temperature", temperature_in_C);
+          request.setAttribute("desp",currentWeather ); 
+          request.setAttribute("humidity", humidity);    
+          request.setAttribute("wind", wind);
+          request.setAttribute("longitude", longt);
+          request.setAttribute("latitude", latitude);
+   // sunrise
+          request.setAttribute("sunrise", sunriseTime);
+          request.setAttribute("sunset", sunsetTime);
+          request.setAttribute("date1", date1); 
+          request.setAttribute("day", day); 
+//          request.setAttribute("icon1", icon1); 
+          
+          request.setAttribute("weatherData", saveResponse.toString());
+          request.setAttribute("weatherData1", saveResponse2.toString());
+          
+          request.getRequestDispatcher("internal.jsp").forward(request, response);
+          request.setAttribute("weatherData1", saveResponse2.toString());
+          
         //5 days 3 hours interval forecast
- 
-	doGet(request, response);
+//        String for_url="api.openweathermap.org/data/2.5/forecast?q="+userInput+"&appid="+key;
+//
+//        URL forapiURL=new URL (for_url);
+        // putting all the data into a list
+       
+//        
+//      HttpURLConnection connection1 = (HttpURLConnection) forapiURL.openConnection();
+//        connection1.setRequestMethod("GET");
+//     InputStream inputConnection1 = connection1.getInputStream();
+//     InputStreamReader read_information1= new InputStreamReader(inputConnection1);
+//     
+//     
+//     StringBuilder saveResponse1= new StringBuilder();
+//     Scanner scanner1= new Scanner(read_information);
+//     while (scanner1.hasNext()) {
+//     	 saveResponse1.append(scanner.nextLine());
+//     }
+//     scanner.close();
+//     Gson gson1 = new Gson();
+//     JsonObject jsonObject1 = gson.fromJson( saveResponse1.toString(), JsonObject.class);
+//     scanner1.close();
+//     
+//     JsonArray forecastList = JsonParser.parseString(jsonObject1 ).getAsJsonObject().getAsJsonArray("list");
+//
+//// Process forecast data and store in a list of objects
+//     List<Forecast> forecasts = new ArrayList<>();
+//     
+//       for (JsonElement element : forecastList) {
+//       
+//    	   double temperature_in_K1 = jsonObject.getAsJsonObject("main").get("temp").getAsDouble();
+//           int temperature_in_C1 = (int) (temperature_in_K1- 273.15);
+//           request.setAttribute("temperature1", temperature_in_C);
+// 
+//       request.setAttribute("weatherData", saveResponse1.toString());
+//       request.getRequestDispatcher("internal.jsp").forward(request, response);
+       
+		doGet(request, response);
 	}
      
 }
